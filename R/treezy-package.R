e06bd5676c0904093153027170e11d99215a9239
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-12-18 23:58:33 +1000

    reduce to what is needed NAMESPACE, add pipe

--- R/treezy-package.R
+++ R/treezy-package.R
@@ -2,12 +2,7 @@
 #'
 #' @name treezy
 #' @docType package
-#' @importFrom gridExtra grid.arrange
-#' @importFrom tidyr gather
-#' @import dplyr
-#' @import ggplot2
-#' @import reshape2
-#' @importFrom ggalt geom_lollipop
+#' @importFrom magrittr %>%
 NULL
 
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
